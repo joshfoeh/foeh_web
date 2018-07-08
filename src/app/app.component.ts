52e81fcc7c408c73a37d45e9abc417ab7c9af952
Josh Foeh <jfoeh@purdue.edu>
2018-07-08 12:08:59 -0500

    updated favicon again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { SimpleService } from 'src/app/services/simple.service';
+import { Message } from 'src/app/types/Message';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  message: string;
+
+  constructor(private simpleService: SimpleService){}
+
+  ngOnInit(){
+      this.simpleService.getText().subscribe((data: Message) => {
+        this.message = data.message;
+      });
+  }
+
 }
